d7596601d8d17d0b08a8f4d4e3d33e728ae7ee22
Anika Roy <anika1234roy@gmail.com>
2023-08-18 14:24:34 +0530

    unsure about ; and & requirements

--- handle_input_commands.c
+++ handle_input_commands.c
@@ -10,6 +10,11 @@ These cases must be handled:
 4. ‘&’ operator runs the command preceding it in the background after printing the process id of the newly created process.
 */
 
+/*
+ChatGPT usage:
+1. used for implementing point 3 and 4
+*/
+
 void print_tokens(char *tokens[], int i)
 {
     for (int j = 0; j < i; j++)
@@ -42,18 +47,103 @@ char* trim_whitespace(char *str)
     return str;
 }
 
+// Function to execute a command -->   ChatGPT referred
+void execute_seq_command(char* token){
+    // Create a new process using fork
+    pid_t pid = fork();
+
+    if (pid == -1)
+    {
+        perror("fork");
+        exit(EXIT_FAILURE);
+    }
+    else if (pid == 0) // Child process
+    {
+        // Tokenize the command for exec
+        char *command = token;
+        char *args[64]; // Adjust the size if needed
+
+        char *token = strtok(command, " ");
+        int arg_index = 0;
+        while (token != NULL)
+        {
+            args[arg_index++] = token;
+            token = strtok(NULL, " ");
+        }
+        args[arg_index] = NULL;
+
+        // Execute the command using exec
+        execvp(args[0], args);
+        
+        // If execvp returns, an error occurred
+        perror("execvp");
+        exit(EXIT_FAILURE);
+    }
+    else // Parent process
+    {
+        // Wait for the child process to finish
+        int status;
+        waitpid(pid, &status, 0);
+        
+        // Check if the child process exited normally
+        if (WIFEXITED(status))
+        {
+            printf("Command exited normally\n");
+        }
+    }
+}
+
+// Function to execute background commands -->   ChatGPT referred
+void execute_background_commands(char* tokens[], int i){
+    // Create a new process using fork
+    pid_t pid = fork();
+
+    if (pid == -1)
+    {
+        perror("fork");
+        exit(EXIT_FAILURE);
+    }
+    else if (pid == 0) // Child process
+    {
+        // Tokenize the command for exec
+        char *command = tokens[0];
+        char *args[64]; // Adjust the size if needed
+
+        char *token = strtok(command, " ");
+        int arg_index = 0;
+        while (token != NULL)
+        {
+            args[arg_index++] = token;
+            token = strtok(NULL, " ");
+        }
+        args[arg_index] = NULL;
+
+        // Execute the command using exec
+        execvp(args[0], args);
+        
+        // If execvp returns, an error occurred
+        perror("execvp");
+        exit(EXIT_FAILURE);
+    }
+    else // Parent process
+    {
+        // Print the process id of the child process
+        printf("Process id of the child process: %d\n", pid);
+    }
+}
+
 void handle_input_commands(char* input){
 
     //implement Specification2
     // point 1 and store these tokens for later use
-    char *token = strtok(input, ";&");
+    char *token = strtok(input, ";");
     //store tokens
     char *tokens[1024];
     int i = 0;
     while (token != NULL)
     {
         tokens[i] = token;
-        token = strtok(NULL, ";&");
+        token = strtok(NULL, ";");
         i++;
     }
 
@@ -68,10 +158,43 @@ void handle_input_commands(char* input){
 
     print_tokens(tokens, i);
 
-    // point 3
+    // point 3 and 4
+    // implement sequential and background commands
 
-    
-    // point 4
+    char* tokens_with_background[1024];
+
+    // Process each token to check for background execution
+    for (int j = 0; j < i; j++)
+    {
+        // Check for background execution
+        char *background_token = strstr(tokens[j], "&");
+
+        if(background_token!=NULL){
+            // tokenise the token removing the '&' character
+            int i_with_background = 0;
+            while (background_token != NULL)
+            {
+                tokens_with_background[i_with_background] = background_token;
+                token = strtok(NULL, "&");
+                i_with_background++;
+            }
+
+            // Trim leading and trailing whitespace from the token
+            for (int k = 0; k < i_with_background; k++)
+            {
+                tokens_with_background[j]=trim_whitespace(tokens_with_background[j]);
+            }
+            // execute the background commands
+            execute_background_commands(tokens_with_background, i_with_background);
+            
+        }
+
+        else{
+            // execute the sequential command
+            execute_seq_command(tokens[j]);
+        }
+
+    }
     
 
 
--- handle_input_commands.h
+++ handle_input_commands.h
@@ -2,6 +2,8 @@
 #define HANDLE_INPUT_COMMANDS_H_
 
 #include "headers.h"
+#include <sys/types.h>
+#include <sys/wait.h>
 
 void handle_input_commands(char *input);
 
